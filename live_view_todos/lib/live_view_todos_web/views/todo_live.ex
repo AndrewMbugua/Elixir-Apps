153669d95ab6c8f54f8c21fbc4d531dcf3d69221
Andrew Mbugua <andrewmbugua388@gmail.com>
2021-12-16 11:30:25 +0300

    Screenshots,new code added

--- live_view_todos/lib/live_view_todos_web/views/todo_live.ex
+++ live_view_todos/lib/live_view_todos_web/views/todo_live.ex
@@ -2,10 +2,17 @@ defmodule LiveViewTodosWeb.TodoLive do
 use LiveViewTodosWeb, :live_view
 alias LiveViewTodos.Todos
 def mount(_params, _session, socket)do
-{:ok, assign(socket, todos: Todos.list_todos())}
+{:ok, fetch(socket)}
 end
 
-def render(assigns)do
-~L"Rendering LiveView"
+def handle_event("add", %{"todo" => todo}, socket) do
+Todos.create_todo(todo)
+
+{:noreply, fetch(socket)}
+end
+
+#Create a new handle event function,that watches for an add event
+defp fetch(socket) do
+  assign(socket, todos: Todos.list_todos())
 end
 end

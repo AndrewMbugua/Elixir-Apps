642df0af58b72dd6775de24b755e9577a70efa05
Andrew Mbugua <andrewmbugua388@gmail.com>
2022-01-14 09:34:06 +0300

    exercise_2

--- Exercises/Exercise_2/counter/lib/counter.ex
+++ Exercises/Exercise_2/counter/lib/counter.ex
@@ -8,7 +8,15 @@ list = ["cow", "goat", "sheep"]
 Enum.map(list, &String.length/1)
 end
 
-#Method 2
+#Using graphemes
+def grapheme do
+list = ["cow", "goat", "sheep"]
+
+Enum.map(list, &String.graphemes/1)
+end
+
+
+#Method 2 - Provide the list elements through a function
 def counter1(list1) do
 
 Enum.map(list1, &String.length/1)

40ba67bcffe8ea85b6d570606c8e36a9ba6e6f6c
Andrew Mbugua <andrewmbugua388@gmail.com>
2022-01-15 00:51:45 +0300

    exercise 1 correction addition

--- Exercises/Exercise_2/counter/lib/counter.ex
+++ Exercises/Exercise_2/counter/lib/counter.ex
@@ -11,16 +11,25 @@ end
 #Using graphemes
 def grapheme do
 list = ["cow", "goat", "sheep"]
-
 Enum.map(list, &String.graphemes/1)
 end
 
 
 #Method 2 - Provide the list through a function
 def count(list) do
-
 Enum.map(list, &String.length/1)
 end
+
+
+#Exercise_1 correction
+#input: ["cow", "goat", "sheep"]
+#Output: ["COW", "GOAT", "SHEEP"]
+def capitalize(input) do
+Enum.map(input, &String.upcase/1)
+
+end
+
+
 
 
 end

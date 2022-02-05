737ba726fc186f8f394447575929c243f9f4e467
Andrew <andrewmbugua388@gmail.com>
2022-02-05 23:39:16 +0300

    New changes

--- Alchemist_Camp/Lesson 1: The guessing game/guessing_game/lib/guessing_game.ex
+++ Alchemist_Camp/Lesson 1: The guessing game/guessing_game/lib/guessing_game.ex
@@ -1,10 +1,15 @@
 defmodule GuessingGame do
 
+  # returns midpoint
   def mid(low,high) do
-
-    div(low + high,2)
-
-
+    div(low + high, 2)
 
   end
+
+    def bigger(low,high)do
+      new_low = min high,mid(low,high) + 1 # min finds the smallest of 2 numbers
+
+    end
+
+
   end

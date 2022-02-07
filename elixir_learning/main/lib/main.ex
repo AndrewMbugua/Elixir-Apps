ce5841e30a0d77916539e6db706e90c41c7b1c29
Andrew <andrewmbugua388@gmail.com>
2022-02-07 08:27:12 +0300

    Elixir Learning

---
+++ elixir_learning/main/lib/main.ex
@@ -0,0 +1,22 @@
+defmodule Main do
+
+def chunk_by do
+
+list = [ "one", "two", "three", "four", "five" ]
+func_length = fn(x) -> String.length(x) end
+
+Enum.chunk_by(list, func_length)
+
+end
+
+def map_every do
+list = [1,2,3,4,5,6,7,8]
+
+Enum.map_every(list, 3 , fn(x) -> x + 1000 end)
+
+#1001, 2 ,3 ,1004,5 , 6, 1007 ,8
+
+end
+
+
+ end

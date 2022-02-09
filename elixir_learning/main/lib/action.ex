06faf83fffb41cf6e2cd25056714061ebb488a3a
Andrew <andrewmbugua388@gmail.com>
2022-02-09 15:57:14 +0300

    *push

--- elixir_learning/main/lib/action.ex
+++ elixir_learning/main/lib/action.ex
@@ -3,18 +3,9 @@ defmodule Action do
 
 def module_1 do
 
-  #person = ["bob","Andrew","John",10,20]
+  
 
-  person = [ ["Head","pre-pend"] ++ ["bob","Andrew","John",10,20] ]
-
-  IO.puts(Enum.at(person,0))
 end
-
-  def concurrency do
-
-    Enum.each(1..5, &async_query.("query #{&1}"))
-    :ok
-  end
 
 
 end

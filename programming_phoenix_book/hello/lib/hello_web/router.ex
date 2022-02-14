8571d16e06a353cdcc52de00e7f0218aa43a03cd
Andrew <andrewmbugua388@gmail.com>
2022-02-14 14:03:47 +0300

    router.ex

--- programming_phoenix_book/hello/lib/hello_web/router.ex
+++ programming_phoenix_book/hello/lib/hello_web/router.ex
@@ -9,13 +9,13 @@ defmodule HelloWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
-
+ 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", HelloWeb do
-    pipe_through :browser
+  scope "/hello", HelloWeb do
+    pipe_through :browser #handles house keeping
 
     get "/", PageController, :index
   end

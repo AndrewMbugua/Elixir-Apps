35cb8fc3b31ee67606de7c6e51b0f19b0692be75
Andrew <andrewmbugua388@gmail.com>
2022-02-14 15:08:09 +0300

    inspect

--- Alchemist_Camp/Lesson 2: Word Count/word_count.exs
+++ Alchemist_Camp/Lesson 2: Word Count/word_count.exs
@@ -1,5 +1,8 @@
 # exs are files meant for scripting 
 # ex are meant to be compiled
+# String.trim gets rid of the white spaces
 
-filename = IO.gets("File to count the words from:")
-IO.puts (filename)
+filename = IO.gets("File to count the words from:") |> String.trim()
+
+file_body = File.read!(filename)
+IO.inspect (file_body)

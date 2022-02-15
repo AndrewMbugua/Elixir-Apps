894472f0bba877c2ce8904457e90f117b50e9d2f
Andrew <andrewmbugua388@gmail.com>
2022-02-15 15:13:10 +0300

    Word count

--- Alchemist_Camp/Lesson 2: Word Count/word_count.exs
+++ Alchemist_Camp/Lesson 2: Word Count/word_count.exs
@@ -1,11 +1,12 @@
 # exs are files meant for scripting 
 # ex are meant to be compiled
 # String.trim gets rid of the white spaces
-
 filename = IO.gets("File to count the words from:") |> String.trim()
 
-file_body = String.split(File.read!(filename), ~r{^[\w]}) # matches everything except a word character
+file_body = String.split(File.read!(filename), ~r{\\n | [^\w']+})
+|> Enum.filter(fn x -> x != "" end)# matches everything except a word character
 
+IO.inspect(file_body)
 
-
-IO.inspect (file_body)
+file_body |> Enum.count() |> IO.puts
+  
--- Exercises/Exercise_2/counter/lib/counter.ex
+++ Exercises/Exercise_2/counter/lib/counter.ex
@@ -1,6 +1,7 @@
 defmodule Counter do
 
 #list ["cow", "goat", "sheep"]
+
 def count(list) do
 Enum.map(list, &String.length/1)
 end
@@ -9,6 +10,7 @@ end
 #Exercise_1 correction
 #input: ["cow", "goat", "sheep"]
 #Output: ["COW", "GOAT", "SHEEP"]
+
 def capitalize(input) do
 Enum.map(input, &String.upcase/1)
 

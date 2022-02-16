18d0ca8ad7b255503240f04e9f992b61c196724f
Andrew <andrewmbugua388@gmail.com>
2022-02-16 12:18:21 +0300

    k

--- elixir_learning/main/lib/action.ex
+++ elixir_learning/main/lib/action.ex
@@ -4,12 +4,15 @@ defmodule Action do
 def practise do
 #~c(Expressed by: #{3 + 0.14159}) 
 #"string"  <> " " <>  "concatenation"
-Enum.each(1..3, &IO.puts/1)
+#Enum.each(1..3, &IO.puts/1)
 
 end
 
 def control_flow do
-    
+    #a = 1 #variable a is bound to 1
+
+{date,time} = :calendar.local_time()
+{year,month,day} = date
 
 
 end

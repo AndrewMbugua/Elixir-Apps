bca00079fa0fda18e8b7512dbce555f45595f559
Andrew <andrewmbugua388@gmail.com>
2022-02-17 09:08:36 +0300

    added view

--- programming_phoenix_book/hello/lib/hello_web/controllers/hello_controller.ex
+++ programming_phoenix_book/hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,5 +1,5 @@
 defmodule HelloWeb.HelloController do
-use HelloWeb, :controller #prepares us to use Phoenix's controller API
+use HelloWeb, :controller  #prepares us to use Phoenix's controller API
 
 def world(conn, _params) do
 render(conn, "world.html")

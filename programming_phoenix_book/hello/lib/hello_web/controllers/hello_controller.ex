a5317dccb1c2a564744b335c7978acb2d9e2162b
AndrewMbugua <andrewmbugua388@gmail.com>
2022-02-18 16:04:41 +0300

    Scripted auto-commit on change (2022-02-18 16:04:41) by gitwatch.sh

--- programming_phoenix_book/hello/lib/hello_web/controllers/hello_controller.ex
+++ programming_phoenix_book/hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,6 +1,7 @@
 defmodule HelloWeb.HelloController do
 use HelloWeb, :controller  #prepares us to use Phoenix's controller API
 alias  Hello.Accounts
+
 def world(conn, %{"name" => name})do
 
   Hello.Accounts.list()

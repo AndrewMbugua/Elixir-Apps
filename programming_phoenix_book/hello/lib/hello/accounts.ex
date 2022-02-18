fceae832b4025b32a88965c114aeda9aa3a862a0
AndrewMbugua <andrewmbugua388@gmail.com>
2022-02-18 15:59:30 +0300

    Scripted auto-commit on change (2022-02-18 15:59:30) by gitwatch.sh

--- programming_phoenix_book/hello/lib/hello/accounts.ex
+++ programming_phoenix_book/hello/lib/hello/accounts.ex
@@ -1,7 +1,10 @@
 defmodule Hello.Accounts do
 
   def list do
-    
+
+    [
+      %User{id: "1", name: "José", username: "josevalim"},
+      %User{id: "2", name: "Bruce", username: "redrapids"}]
 
   end
 

a98c4a3286421990b0e410ace676a7c0170c4e14
Andrew <andrewmbugua388@gmail.com>
2022-02-21 12:27:50 +0300

    Accounts added

---
+++ programming_phoenix_book/rumbl/lib/rumbl/accounts.ex
@@ -0,0 +1,29 @@
+defmodule Rumbl.Accounts do
+# Accounts context - Groups together related functions
+
+alias Rumbl.Accounts.User
+
+# mapping to the struct
+# we start by hardcoding,then later map to a db
+def list_users do
+[
+
+   %User{:id => "1", :name => "Jose", :username => "JoseValim"},
+   %User{:id => "2", :name => "Andrew", :username => "Andy"},
+   %User{:id => "3", :name => "Johnny", :username => "John"}
+]
+end
+
+def get_user(id)do
+  #Enum.find returns 1st element which is true
+  Enum.find(list_users(),fn map -> map.id == id end)
+end
+
+def get_user_by(params) do
+  Enum.find(list_users(), fn map ->
+                           Enum.all?(params, fn {key,val} -> Map.get(map, key) == val end)end)
+end
+
+
+
+end
---
+++ programming_phoenix_book/rumbl/lib/rumbl/accounts/user.ex
@@ -0,0 +1,5 @@
+defmodule Rumbl.Accounts.User do
+
+  defstruct [:id, :name, :username]
+
+end

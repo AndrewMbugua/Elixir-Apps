eb809b17ea3de28a734bc90c34b6ae2651de4ab1
Andrew <andrewmbugua388@gmail.com>
2022-02-22 13:36:06 +0300

    push more changes

--- programming_phoenix_book/rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ programming_phoenix_book/rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -8,4 +8,11 @@ users = Accounts.list_users()
 render(conn, "index.html", users: users)
 end
 
+# displays users by id
+def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
+
+end
+
 end

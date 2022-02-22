6b21f2119031247fd4c1d32c1dc210d1604f35b4
Andrew <andrewmbugua388@gmail.com>
2022-02-22 20:17:56 +0300

    Submit

--- elixir_learning/main/lib/school.ex
+++ elixir_learning/main/lib/school.ex
@@ -1,5 +1,16 @@
 defmodule School do
 
-  
+  def is_palindrome(s) do
+    
+    reverse = String.reverse(s)
+    if reverse == is_palindrome(s) do
+    true
 
+    else
+    
+    false
+    end
+
+
+end
 end

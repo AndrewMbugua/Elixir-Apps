667b04518037ac26f497c7b3f65d80ff11eea922
Andrew <andrewmbugua388@gmail.com>
2022-02-28 21:54:11 +0300

    push

--- elixir_learning/main/lib/school.ex
+++ elixir_learning/main/lib/school.ex
@@ -1,14 +1,11 @@
 defmodule School do
-  # protects a particular field in the struct
-  @derive {Inspect, only: [:name]}
-   defstruct name: nil , roles: [:manager, :developer] 
- 
-def module do
   
+def module do
+
+
+
+
   
 end
-
-
-
 
 end

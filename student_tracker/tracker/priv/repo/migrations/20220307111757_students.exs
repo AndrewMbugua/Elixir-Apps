2eb32afd472523e7b6b738a187d887d7ba8f5874
Andrew <andrewmbugua388@gmail.com>
2022-03-07 17:23:42 +0300

    New migration generated

--- student_tracker/tracker/priv/repo/migrations/20220307111757_students.exs
+++ student_tracker/tracker/priv/repo/migrations/20220307111757_students.exs
@@ -2,6 +2,14 @@ defmodule Tracker.Repo.Migrations.Students do
   use Ecto.Migration
 
   def change do
+   create table(:students)do
+     add :full_name, :string
+     add :admission_number, :string
+     add :date_of_joining, :utc_datetime
 
+     timestamps()
+   end
+
+   create unique_index(:students, [:admission_number])
   end
 end

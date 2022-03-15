96b89c2c984178fc88d791d19b7623a77136bcbe
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-15 13:10:05 +0300

    Scripted auto-commit on change (2022-03-15 13:10:05) by gitwatch.sh

--- elixir_learning/main/lib/CLI.ex
+++ elixir_learning/main/lib/CLI.ex
@@ -10,7 +10,7 @@ end
 defp parse_args(args)do
 {opts, word, _} =
   args
-  |> OptionParser.parse(switches: [upcas: :boolean])
+  |> OptionParser.parse(switches: [upper: :boolean])
 
   {opts, List.to_string(word)}
 end

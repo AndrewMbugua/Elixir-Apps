05d56053738eb254011684f34bbcbea59fedcc5f
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-16 15:12:18 +0300

    Scripted auto-commit on change (2022-03-16 15:12:18) by gitwatch.sh

--- json_api/firehose/lib/firehose_web/router.ex
+++ json_api/firehose/lib/firehose_web/router.ex
@@ -24,7 +24,7 @@ defmodule FirehoseWeb.Router do
    scope "/api", FirehoseWeb do
      pipe_through :api
 
-     get "/roll"
+     get "/roll", RollController
    end
 
   # Enables LiveDashboard only for development

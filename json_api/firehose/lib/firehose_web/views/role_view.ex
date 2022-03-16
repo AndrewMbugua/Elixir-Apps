51924f1dc8774464a1297b57d1c90370d081cea7
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-16 15:29:02 +0300

    Scripted auto-commit on change (2022-03-16 15:29:02) by gitwatch.sh

--- json_api/firehose/lib/firehose_web/views/role_view.ex
+++ json_api/firehose/lib/firehose_web/views/role_view.ex
@@ -1,5 +1,5 @@
 # with JSON responses we write the render functions in the view directly instead of writing a template
-defmodule FirehoseWeb.RoleView do
+defmodule FirehoseWeb.RollView do
 use FirehoseWeb, :view
 
 def render("index.json", %{roll: num}) when is_integer(num) do
@@ -8,4 +8,3 @@ end
 
 def render("index.json", _), do: %{status: "error"}
 end
-

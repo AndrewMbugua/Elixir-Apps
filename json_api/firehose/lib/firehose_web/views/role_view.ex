d2631a7b3fa46a1b8b9abd908ba190f1ecf2fa2b
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-16 15:24:12 +0300

    Scripted auto-commit on change (2022-03-16 15:24:12) by gitwatch.sh

--- json_api/firehose/lib/firehose_web/views/role_view.ex
+++ json_api/firehose/lib/firehose_web/views/role_view.ex
@@ -1,4 +1,4 @@
-# with JSON responses we write the render functions in the view directly instead of writing o
+# with JSON responses we write the render functions in the view directly instead of writing a template
 defmodule FirehoseWeb.RoleView do
 use FirehoseWeb, :view
 end

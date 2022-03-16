3811979c554d15a404da8cf00b6d6adaa867e454
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-16 15:20:53 +0300

    Scripted auto-commit on change (2022-03-16 15:20:53) by gitwatch.sh

--- json_api/firehose/lib/firehose_web/controllers/roll_controller.ex
+++ json_api/firehose/lib/firehose_web/controllers/roll_controller.ex
@@ -2,7 +2,7 @@ defmodule FirehoseWeb.RollController do
   use FirehoseWeb, :controller
 
   def index(conn,_params) do
-    num = :rand.uniform(6) #randomly gner
+    num = :rand.uniform(6) #randomly generated n.o from 1..6
     render(conn, "index.json",roll: num)
   end
 

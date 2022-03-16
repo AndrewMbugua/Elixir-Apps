9fa180052c2d1ef4de47be7e4ec8d06e2af6fa3c
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-16 14:33:27 +0300

    Scripted auto-commit on change (2022-03-16 14:33:27) by gitwatch.sh

--- Alchemist_Camp/Lesson 3: JSON API/firehose/lib/firehose_web/router.ex
+++ Alchemist_Camp/Lesson 3: JSON API/firehose/lib/firehose_web/router.ex
@@ -22,7 +22,7 @@ defmodule FirehoseWeb.Router do
 
   # Other scopes may use custom stacks.
    scope "/api", FirehoseWeb do
-     pipe_through :api #calls all plugs 
+     pipe_through :api #calls all plugs in the :api
 
      get "/roll", RollController, :index
    end

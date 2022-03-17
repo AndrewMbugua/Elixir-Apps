aaeb1c75be9348af5fc0cc5b4c194e4b5c0279f1
Andrew <andrewmbugua388@gmail.com>
2022-03-17 16:46:29 +0300

    push

--- elixir_learning/main/lib/main.ex
+++ elixir_learning/main/lib/main.ex
@@ -1,57 +1,18 @@
 defmodule Main do
 
-def chunk_by do
+def error_handling do
 
-list = [ "one", "two", "three", "four", "five" ]
-func_length = fn(x) -> String.length(x) end
+  try do
+    raise "OH NO!"
+  rescue
+    
 
-Enum.chunk_by(list, func_length)
+
+  end
+
+
+
 
 end
-
-def map_every do
-list = [1,2,3,4,5,6,7,8]
-
-Enum.map_every(list, 3 , fn(x) -> x + 1000 end)
-
-#1001, 2 ,3 ,1004,5 , 6, 1007 ,8
-
-end
-
-def filter do
-
-Enum.filter([1,2,3,4], fn(x) -> rem(x,2) == 0 end)
-
-end
-
-def reduce do
-
-  #Enum.reduce([1,2,3], 10 , fn(x ,acc) -> x + acc end)
-
-  Enum.reduce(["a","b","c"], fn(x ,acc) -> x <> acc end)
-
-end
-
-def pin_operator do
-
-key = "Hello"
-
-%{^key => value} = %{"Hello" => "World"}
-
-value
-end
-
-def control_structures do
-
-case {:ok, "Hello World"} do
-  {:ok, result} -> result
-  {:error} -> "Uh oh!"
-  _ -> "Catch all"
-end
-"Hello World"
-
-end
-
-
 
  end

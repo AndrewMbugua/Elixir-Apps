397d58428d3c31211baaa6701efdd5ad0b4463b5
Andrew <andrewmbugua388@gmail.com>
2022-03-17 18:55:28 +0300

    push

--- elixir_learning/main/lib/main.ex
+++ elixir_learning/main/lib/main.ex
@@ -1,18 +1,30 @@
 defmodule Main do
 
-def error_handling do
+def error_handling_1 do
 
   try do
     raise "OH NO!"
   rescue
-    
+    e in RuntimeError -> IO.puts("An error occured:" <> e.message)
 
-
+  after
+    IO.puts("The End!")
   end
+end
+
+def error_handling_2 do
+ try do
+ for x <- 0..10 do
 
 
+
+
+ end
+ end
 
 
 end
+
+
 
  end

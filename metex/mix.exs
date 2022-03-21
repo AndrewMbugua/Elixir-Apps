e473ad2c852b04dd02e1fb0fc255640c59687747
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-21 23:11:14 +0300

    Scripted auto-commit on change (2022-03-21 23:11:14) by gitwatch.sh

--- metex/mix.exs
+++ metex/mix.exs
@@ -14,7 +14,7 @@ defmodule Metex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :htt]
+      extra_applications: [:logger, :httpoison]
     ]
   end
 

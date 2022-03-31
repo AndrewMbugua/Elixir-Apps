8d6a3a332bb7eb51ee6555b7fd7788ca785d0826
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-31 15:15:52 +0300

    Scripted auto-commit on change (2022-03-31 15:15:52) by gitwatch.sh

--- LiveView/pento/lib/pento_web/live/wrong_live.ex
+++ LiveView/pento/lib/pento_web/live/wrong_live.ex
@@ -1,7 +1,7 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
-def mount(_params,_session, socket)
+def mount(_params,_session, socket) do
 {
   :ok,
   assign(socket, score: 0, message: "Guess a number.")

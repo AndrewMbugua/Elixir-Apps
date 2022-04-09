90b294c08999f3a2adea2c0d1d7a02be13eb6373
Andrew <andrewmbugua388@gmail.com>
2022-04-09 18:00:05 +0300

    oush

--- LiveView/pento/lib/pento_web/live/wrong_live.ex
+++ LiveView/pento/lib/pento_web/live/wrong_live.ex
@@ -4,7 +4,7 @@ defmodule PentoWeb.WrongLive do
 def mount(_params,_session, socket) do
 {
   :ok,
-  assign(socket, score: 1, message: "Guess a number.")
+  assign(socket, score: 10, message: "Guess a number.")
 
 }
 end
@@ -33,6 +33,7 @@ end
 def handle_event("guess", data = %{"number" => guess}, socket) do
    IO.inspect data
    message = "Your guess: #{guess}, Wrong. Guess again."
+
    score = socket.assigns.score - 1
 
   {
---
+++ SQL executer/README.md
@@ -0,0 +1,3 @@
+## A web based SQL query executer,a web app that connects to a postgredb
+
+An API
